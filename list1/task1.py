7770fddbcdf0459fde6657e0380f0b23098b8e33
luk9400 <lukaszbratos@gmail.com>
2020-03-03 10:57:39 +0100

    hmm

--- list1/task1.py
+++ list1/task1.py
@@ -1,9 +1,14 @@
 import math
+import sys
 
 def func(filename):
     with open(filename, 'rb') as f:
         symbols_frequency = {}
+        b = {}
         num_of_symbols = 0
+        prev_symbol = chr(0)
+        b[prev_symbol] = {}
+
         while True:
             symbol = f.read(1)
             if not symbol:
@@ -11,11 +16,23 @@ def func(filename):
 
             num_of_symbols += 1
 
+            if prev_symbol not in b:
+                b[prev_symbol] = {}
+
+            if symbol in b[prev_symbol]:
+                b[prev_symbol][symbol] += 1
+            else:
+                b[prev_symbol][symbol] = 1
+
+            prev_symbol = symbol
+
             if symbol in symbols_frequency:
                 symbols_frequency[symbol] += 1
             else:
                 symbols_frequency[symbol] = 1
+            
         print(symbols_frequency)
+        print(b)
         print(entropy(symbols_frequency, num_of_symbols))
 
 def entropy(freq, num_of_symbols):
@@ -24,4 +41,9 @@ def entropy(freq, num_of_symbols):
         H += freq[i]/num_of_symbols * -math.log(freq[i]/num_of_symbols, 2)
     return H
 
-func('test2.bin')
+def cond_entropy(cond_freq, freq):
+    pass
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    func(filename)
