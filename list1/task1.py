4a7e749a237815ef25c502c803309bcb1ec6cd2b
luk9400 <lukaszbratos@gmail.com>
2020-03-04 18:00:22 +0100

    task1

--- list1/task1.py
+++ list1/task1.py
@@ -1,13 +1,14 @@
 import math
 import sys
 
+
 def func(filename):
     with open(filename, 'rb') as f:
         symbols_frequency = {}
-        b = {}
+        cond_freq = {}
         num_of_symbols = 0
-        prev_symbol = chr(0)
-        b[prev_symbol] = {}
+        prev_symbol = b'\x00'
+        cond_freq[prev_symbol] = {}
 
         while True:
             symbol = f.read(1)
@@ -16,13 +17,13 @@ def func(filename):
 
             num_of_symbols += 1
 
-            if prev_symbol not in b:
-                b[prev_symbol] = {}
+            if prev_symbol not in cond_freq:
+                cond_freq[prev_symbol] = {}
 
-            if symbol in b[prev_symbol]:
-                b[prev_symbol][symbol] += 1
+            if symbol in cond_freq[prev_symbol]:
+                cond_freq[prev_symbol][symbol] += 1
             else:
-                b[prev_symbol][symbol] = 1
+                cond_freq[prev_symbol][symbol] = 1
 
             prev_symbol = symbol
 
@@ -30,10 +31,12 @@ def func(filename):
                 symbols_frequency[symbol] += 1
             else:
                 symbols_frequency[symbol] = 1
-            
-        print(symbols_frequency)
-        print(b)
+
+        # print(symbols_frequency)
+        # print(cond_freq)
         print(entropy(symbols_frequency, num_of_symbols))
+        print(cond_entropy(cond_freq, symbols_frequency, num_of_symbols))
+
 
 def entropy(freq, num_of_symbols):
     H = 0
@@ -41,8 +44,14 @@ def entropy(freq, num_of_symbols):
         H += freq[i]/num_of_symbols * -math.log(freq[i]/num_of_symbols, 2)
     return H
 
-def cond_entropy(cond_freq, freq):
-    pass
+
+def cond_entropy(cond_freq, freq, num_of_symbols):
+    H = 0
+    for i in freq:
+        H += freq[i]/num_of_symbols * sum(cond_freq[i][j]/freq[i] * -math.log(
+            cond_freq[i][j]/freq[i], 2) for j in cond_freq[i])
+    return H
+
 
 if __name__ == '__main__':
     filename = sys.argv[1]
