1f6ea2c46e6cc763f538f5a3a3c3a5a297fd20b5
luk9400 <lukaszbratos@gmail.com>
2020-06-03 19:16:40 +0200

    1/2 listy 7

---
+++ list7/checker.py
@@ -0,0 +1,50 @@
+""" Program prównujący dwa pliki. Wypisuje ile 4-bitowych bloków jest różnych """
+
+from sys import argv
+
+
+def check(in1, in2):
+    hex1 = in1.read().hex()
+    hex2 = in2.read().hex()
+
+    if len(hex1) != len(hex2):
+        print("Pliki są różnej długości")
+        return
+
+    bits1 = "".join(
+        [
+            "{0:08b}".format(int(hex1[x : x + 2], base=16))
+            for x in range(0, len(hex1), 2)
+        ]
+    )
+
+    bits2 = "".join(
+        [
+            "{0:08b}".format(int(hex2[x : x + 2], base=16))
+            for x in range(0, len(hex2), 2)
+        ]
+    )
+
+    diffs = 0
+    for i in range(0, len(bits1), 4):
+        if bits1[i : i + 4] != bits2[i : i + 4]:
+            diffs += 1
+
+    if diffs == 0:
+        print("Pliki są takie same")
+        return
+    else:
+        print(f"Pliki różnią się w {diffs} 4-bitowych blokach")
+
+
+def main():
+    if len(argv) == 3:
+        in1 = argv[1]
+        in2 = argv[2]
+
+        with open(in1, "rb") as f1, open(in2, "rb") as f2:
+            check(f1, f2)
+
+
+if __name__ == "__main__":
+    main()
---
+++ list7/noise.py
@@ -0,0 +1,44 @@
+""" Program z prawdopodobieństwem p zamienia na przeciwny każdy bit z pliku in i zapisuje wynik w pliku out """
+
+from random import random
+from sys import argv
+
+
+def swap(bit):
+    return 1 if bit == "0" else 0
+
+
+def make_some_noise(input_file, output_file, p):
+    with open(input_file, "rb") as f, open(output_file, "wb") as out:
+        payload = f.read()
+
+        hexstring = payload.hex()
+        bitstring = "".join(
+            [
+                "{0:08b}".format(int(hexstring[x : x + 2], base=16))
+                for x in range(0, len(hexstring), 2)
+            ]
+        )
+
+        new_bitstring = ""
+        for bit in bitstring:
+            if p < random():
+                new_bitstring += str(swap(bit))
+
+        b = bytes(
+            int(new_bitstring[i : i + 8], 2) for i in range(0, len(new_bitstring), 8)
+        )
+        out.write(b)
+
+
+def main():
+    if len(argv) == 4:
+        p = float(argv[1])
+        input_file = argv[2]
+        output_file = argv[3]
+
+        make_some_noise(input_file, output_file, p)
+
+
+if __name__ == "__main__":
+    main()
